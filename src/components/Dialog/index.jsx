a096975f630ef642a84c59e6b9d291f5bc217d02
CLEVERSON  SANTOS <felipecleverson1@gmail.com>
2026-02-05 12:43:10 -0400

    chore: initial setup

---
+++ src/components/Dialog/index.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./dialog.style.css";
+
+export function Dialog() {
+    const dialog = document.querySelector("dialog");
+    
+    const openDialog = () => {
+      dialog.showModal();
+    };
+
+    const closeDialog = () => {
+      dialog.close();
+    };
+  return (
+    <>
+      <dialog>
+        <button autoFocus onClick={closeDialog}>
+          Close
+        </button>
+        <p>This modal dialog has a groovy backdrop!</p>
+      </dialog>
+      <button onClick={openDialog}>Show the dialog</button>
+    </>
+  );
+}
